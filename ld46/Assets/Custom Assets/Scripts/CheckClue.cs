0d3bc3f4f2874e12866638b7108a0eb4a69cb6f0
no <noemie.scherer@gmail.com>
2020-04-18 21:58:46 +0200

    try different tests for fungus' dialogue system

---
+++ ld46/Assets/Custom Assets/Scripts/CheckClue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CheckClue : MonoBehaviour
+{
+    Flowchart Flowchart;
+
+    private void Start()
+    {
+        Flowchart = gameObject.GetComponent<Flowchart>();
+    }
+
+    public void CheckForClues()
+    {
+        switch (Flowchart.GetStringVariable("ActiveCharacter"))
+        {
+            case "MusicListener":
+                switch (Flowchart.GetStringVariable("ActiveClue"))
+                {
+                    case "Sam":
+                        Flowchart.SetStringVariable("ActiveClue", "");
+                        Flowchart.StopAllBlocks();
+                        Flowchart.ExecuteBlock("Music Listener - Sam (Copy)1");
+                        break;
+                    case "Pirate":
+                        Flowchart.SetStringVariable("ActiveClue", "");
+                        Flowchart.StopAllBlocks();
+                        Flowchart.ExecuteBlock("Music Listener - Pirate (Copy)1");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+    }
+
+    public void CheckForClues2()
+    {
+        if(CluesQuestion.ActiveClue != Clues.None)
+        {
+            Flowchart.StopAllBlocks();
+            Flowchart.ExecuteBlock(CluesQuestion.ActiveClue.ToString());
+            CluesQuestion.ActiveClue = Clues.None;
+        }
+    }
+
+}
---
+++ ld46/Assets/Custom Assets/Scripts/CluesQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CluesQuestion : MonoBehaviour
+{
+    public static Clues ActiveClue;
+
+    [SerializeField]
+    private Clues localClue;
+
+
+    private void OnMouseDown()
+    {
+        ActiveClue = localClue;
+    }
+}
+
+public enum Clues
+{
+    None,
+    Sam,
+    Pirate
+}
