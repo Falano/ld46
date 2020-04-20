bf67e2d94ffa770f6fe47e5235aaadacaa17822b
Madison Tobal <mtobalo@thoughtworks.com>
2020-04-21 00:08:18 +0200

    Highlight selections

---
+++ ld46/Assets/Custom Assets/Scripts/HighlightSprite.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightSprite : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    Color defaultColor;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+    }
+
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+        spriteRenderer.color = Color.yellow;
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+        spriteRenderer.color = defaultColor;
+    }
+}
