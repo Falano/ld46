e8a30f7d1bb513b2b4fb7efa4fbc2d179781cb43
Madison Tobal <mtobalo@thoughtworks.com>
2020-04-21 01:45:15 +0200

    Add background manager

---
+++ ld46/Assets/Custom Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject DefaultBackground;
+
+    [SerializeField]
+    GameObject FlashbackInside;
+
+    [SerializeField]
+    GameObject FlashbackOutside;
+
+    [SerializeField]
+    GameObject Clues;
+
+    [SerializeField]
+    List<GameObject> Sprites;
+
+    public void SetFlashbackOutside()
+    {
+        DisableSprites();
+        FlashbackOutside.SetActive(true);
+        FlashbackInside.SetActive(false);
+        DefaultBackground.SetActive(false);
+    }
+
+    public void SetFlashbackInside()
+    {
+        Debug.Log("Flashback inside!");
+        DisableSprites();
+        FlashbackOutside.SetActive(false);
+        FlashbackInside.SetActive(true);
+        DefaultBackground.SetActive(false);
+    }
+
+    public void SetDefaultBackground()
+    {
+        EnableSprites();
+        FlashbackOutside.SetActive(false);
+        FlashbackInside.SetActive(false);
+        DefaultBackground.SetActive(true);
+    }
+
+    private void EnableSprites()
+    {
+        Clues.SetActive(true);
+        foreach (var sprite in Sprites)
+        {
+            sprite.SetActive(true);
+        }
+    }
+
+    private void DisableSprites()
+    {
+        Clues.SetActive(false);
+        foreach (var sprite in Sprites)
+        {
+            sprite.SetActive(false);
+        }
+    }
+}
